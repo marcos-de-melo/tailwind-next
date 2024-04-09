227a0525a4b044ddee006106e88c5961e7dfbd22
Marcos de Melo <marcdmelo@gmail.com>
2024-04-09 13:01:28 -0300

    Animação das Abas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import * as Tabs from '@radix-ui/react-tabs'
+import { SettingsTabs } from './components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Setting</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
