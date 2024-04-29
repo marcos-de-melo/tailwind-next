975860d09052ed66624609e405dc7bebd5cdbc95
Marcos de Melo <marcdmelo@gmail.com>
2024-04-28 22:41:20 -0300

    Variante ghost dos Botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import { SettingsTabs } from '../components/SettingsTabs'
+import { Button } from '../components/Button'
 import * as Input from '../components/Input'
 import * as FileInput from '../components/Form/FileInput'
-import {
-  Bold,
-  Italic,
-  Link,
-  List,
-  ListOrdered,
-  LogOut,
-  Mail,
-} from 'lucide-react'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { Textarea } from '@/components/Form/Textarea'
@@ -30,19 +23,17 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="primary"
               type="submit"
               form="settings"
               className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
             >
               Save
-            </button>
+            </Button>
           </div>
         </div>
         <form
@@ -222,18 +213,17 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="primary"
+              type="submit"
               form="settings"
               className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
             >
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
